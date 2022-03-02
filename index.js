718a0caf54df9dd7f97fcea4be01cb3f9dae6910
Sharbel <sharbel.sekias@etu.univ-amu.fr>
2022-03-02 18:55:12 +0100

    change add implementation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const readline = require('readline');
 const chalk = require("chalk");
 const fs = require('fs');
-
+var todoTitle = "";
+var todoSubject = "";
+var my_todo = {};
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -10,18 +12,62 @@ const rl = readline.createInterface({
 printStartProgramme();
 
 
+function askForTitle() {
+  return new Promise((resolve, reject) => {
+    rl.question('enter a title: ',(arr)=>{
+      todoTitle = arr;
+      resolve();
+      askForSubject();
+
+    });
+})
+}
+
+function askForSubject() {
+  return new Promise((resolve, reject) => {
+    rl.question('enter a subject: ',(arr)=>{
+      todoSubject = arr;
+      resolve();
+    });
+})
+}
+
+
 rl.on('line', (argument) => {
     console.log("");
     const listArgument = argument.split(" ");
 
     switch(listArgument[0]) {
+      
+        case "add":
+          askForTitle();
+          break;
 
         case "info":
           printAddDetails();
+          console.log("subj = ", todoSubject);
+          console.log("title = ", todoTitle);
+
+
           break;
         
         case "add":
-
+          if(listArgument.length != 3) {
+            console.log(chalk.red("To add new Todo, please respecte the following example :"));
+            console.log(chalk.yellow("add , argument1, argument2"));
+          }
+          else {
+            fs.readFile('data.json','utf8', function readFileCallback(err, data){
+              if (err){
+                  console.log(err);
+              } else {
+                console.log(" data  : ", data);
+              obj = JSON.parse(data); //now it an object
+              obj.table.push({id: 2, square:3}); //add some data
+              json = JSON.stringify(obj); //convert it back to json
+              fs.writeFile('data.json', json, 'utf8', callback); // write it back 
+          }});
+          }
           break;
       
         case "exit":
@@ -50,7 +96,8 @@ function printStartProgramme(){
 }
 
 function createJsonFileIfDontExist() {
-  fs.closeSync(fs.openSync("./dat.json", 'a'));
+  // the 'a' parametre will check if the file already existe, if not it will create one
+  fs.closeSync(fs.openSync("./data.json", 'a'));
 }
 
 createJsonFileIfDontExist();
