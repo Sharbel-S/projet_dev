ef8296550ce2a4614584d7cb85b5b8af2ca83841
Sharbel <sharbel.sekias@etu.univ-amu.fr>
2022-03-13 11:53:23 +0100

    add GET and POST for signInPage

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var mustache = require('mustache-express');
 const app = express();
+var account_model = require('./models/account_model');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,9 +11,24 @@ app.set('view engine', 'html');
 app.set('views', './views');
 
 
-
 app.get('/', function (req, res) {
   res.render('index');
 })
 
-app.listen(3000)
+app.get('/signInPage', (req, res) => {
+  res.render('signInPage');
+});
+
+app.post('/signInPage', (req, res) => {
+  account_model.check_email_password_account(req.body.email, req.body.password).then((response) => {
+    if (response == false) {
+      console.log("not found !");
+    }
+    else {
+      console.log("found !");
+    }
+  }
+  );
+});
+
+app.listen(5000)
