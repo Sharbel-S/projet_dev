9995b4e9a9e336406b8a0fdebb160b7fe1eb6d8e
Yanis KRAIMI <kraimiyanis@gmail.com>
2022-03-23 11:51:56 +0100

    Modification de la fonction askForModify en appelant la fonction checkIfTitleExist

--- cli/view.js
+++ cli/view.js
@@ -11,7 +11,7 @@ var emailLog = "";
 var passwordLog = "";
 var isConnected = false;
 
-exports.printStartProgramme = function(){
+exports.printStartProgramme = function () {
     console.log(chalk.green("/********* Welcome to MY_TODO *********/"));
     console.log("");
     console.log("----------------------------------")
@@ -26,7 +26,7 @@ exports.printStartProgramme = function(){
     console.log("----------------------------------")
 }
 
-exports.printAddDetails = function() {
+exports.printAddDetails = function () {
     console.log("----------------------------------");
     console.log(chalk.yellow("How to add new todo: "))
     console.log();
@@ -36,9 +36,9 @@ exports.printAddDetails = function() {
     console.log();
     console.log(chalk.green("Well done! you have juste added a new todo to your list !"));
     console.log("----------------------------------");
-  }
-  
-  exports.printRemoveDetails = function() {
+}
+
+exports.printRemoveDetails = function () {
     console.log("----------------------------------");
     console.log(chalk.yellow("How to remove a todo: "))
     console.log();
@@ -47,9 +47,9 @@ exports.printAddDetails = function() {
     console.log();
     console.log(chalk.green("Well done! you have juste removed your todo!"));
     console.log("----------------------------------");
-  }
-  
- exports.printListeDetails = function() {
+}
+
+exports.printListeDetails = function () {
     console.log("----------------------------------");
     console.log(chalk.yellow("How to print the list of todos: "))
     console.log();
@@ -57,105 +57,118 @@ exports.printAddDetails = function() {
     console.log();
     console.log(chalk.green("Well done! you have juste printed all your todos!"));
     console.log("----------------------------------");
-  }
+}
 
-  exports.askForTitle = function(rl) {
-      if(isConnected){
+exports.askForTitle = function (rl) {
+    if (isConnected) {
         return new Promise((resolve, reject) => {
             rl.question('enter a title: ', (arr) => {
-              todoTitle = arr;
-              resolve();
-              askForSubject(rl);
+                todoTitle = arr;
+                resolve();
+                askForSubject(rl);
             });
-          })
-      }
-      else{
-          console.log("You must to login !");
-      }
-    
-  }
-  
-  
-  function askForSubject(rl) {
-    return new Promise((resolve, reject) => {
-      rl.question('enter a subject: ', (inputSubject) => {
-        todoSubject = inputSubject;
-        resolve();
-        controler.addDataToDataBase(todoTitle,todoSubject);
-      });
-    })
-  }
-  
-
-  exports.askForModify = function(rl) {
-    if(isConnected){
-    rl.question('enter the title: ', (title) => {
-      todoTitle = title;
-  
-      rl.question('enter the new title: ', (Newtitle) => {
-        todoNewTitle = Newtitle;
-        controler.modifyActualTitle(todoTitle, todoNewTitle);
-      })
-    });
+        })
     }
     else {
         console.log("You must to login !");
     }
-  }
-  
-  
- exports.askForSignup = function(rl) {
-    return new Promise((resolve, reject) => {
-      rl.question('enter an email: ', (email) => {
-        todoEmail = email;
-        resolve();
-        askForPassword(rl);
-      });
-    })
-  }
-  
-  function askForPassword(rl) {
-    return new Promise((resolve, reject) => {
-      rl.question('enter a password: ', (Password) => {
-        todoPassword = Password;
-        resolve();
-        controler.addNewAccountToDataBase(todoEmail, todoPassword);
-      });
-    })
-  }
-  
 
-  exports.askForTitleToRemove = function(rl) {
-    return new Promise((resolve, reject) => {
-      rl.question('enter the title of the todo you want to delete: ', (title) => {
-        resolve();
-        controler.removeTodoFromDataBase(title);
-    
-      });
-    })
-  }
+}
 
-  exports.askForEmailToLogin = function(rl) {
+
+function askForSubject(rl) {
+    return new Promise((resolve, reject) => {
+        rl.question('enter a subject: ', (inputSubject) => {
+            todoSubject = inputSubject;
+            resolve();
+            controler.addDataToDataBase(todoTitle, todoSubject);
+        });
+    })
+}
+
+
+exports.askForModify = function (rl) {
+    if (isConnected) {
+        rl.question('enter the title: ', (title) => {
+            todoTitle = title;
+            controler.checkIfTitleExist(title).then((response) => {
+                if (response) {
+                    rl.question('enter the new title: ', (Newtitle) => {
+                        todoNewTitle = Newtitle;
+                        controler.modifyActualTitle(todoTitle, todoNewTitle);
+                    })
+                }
+                else {
+                    console.log("Title doesn't exit !");
+                }
+            });
+
+
+        });
+    }
+    else {
+        console.log("You must to login !");
+    }
+}
+
+
+exports.askForSignup = function (rl) {
+    return new Promise((resolve, reject) => {
+        rl.question('enter an email: ', (email) => {
+            todoEmail = email;
+            resolve();
+            askForPassword(rl);
+        });
+    })
+}
+
+function askForPassword(rl) {
+    return new Promise((resolve, reject) => {
+        rl.question('enter a password: ', (Password) => {
+            todoPassword = Password;
+            resolve();
+            controler.addNewAccountToDataBase(todoEmail, todoPassword);
+        });
+    })
+}
+
+
+exports.askForTitleToRemove = function (rl) {
+    if (isConnected) {
+        return new Promise((resolve, reject) => {
+            rl.question('enter the title of the todo you want to delete: ', (title) => {
+                resolve();
+                controler.removeTodoFromDataBase(title);
+
+            });
+        })
+    }
+    else {
+        console.log("You must to login !");
+    }
+}
+
+exports.askForEmailToLogin = function (rl) {
     return new Promise((resolve, reject) => {
         rl.question('enter your email: ', (email) => {
-          emailLog = email;
-          resolve();
-          askForPasswordToLogin(rl);
+            emailLog = email;
+            resolve();
+            askForPasswordToLogin(rl);
         });
-      })
-  }
+    })
+}
 
 
-    function askForPasswordToLogin (rl) {
+function askForPasswordToLogin(rl) {
     return new Promise((resolve, reject) => {
         rl.question('enter your password: ', (password) => {
-         passwordLog = password;
-          resolve();
-        controler.checkEmailPasswordAccount(emailLog,passwordLog).then((response) => {
-            if(response == true){
-                isConnected = true;
-            }
+            passwordLog = password;
+            resolve();
+            controler.checkEmailPasswordAccount(emailLog, passwordLog).then((response) => {
+                if (response == true) {
+                    isConnected = true;
+                }
+            });
         });
-        });
-      })
-  }
+    })
+}
